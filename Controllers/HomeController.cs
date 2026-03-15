748d952885b84d2b14e9261e38d29a6c5b44ab23
Anthony Tolley <atolley0217@starkstate.com>
2026-03-14 23:02:43 -0400

    Revert "Added Test"
    This reverts commit 010f1a128dfb038296135101eb4d391c530f823d.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace CollegeMarketplaceMarch2026.Controllers
         //Anthony was here
         //Anthony was here to test pull
         //Twyla test
-        //"I come to bring Despair"
         public ActionResult Index()
         {
             return View();
