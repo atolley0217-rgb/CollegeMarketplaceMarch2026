c7784261c09ad882b5a66013017db399ce06c204
Anthony Tolley <atolley0217@starkstate.com>
2026-03-14 23:55:59 -0400

    - Made major models to be used in the retreival of db data

---
+++ Models/TransactionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CollegeMarketplaceMarch2026.Models
+{
+    public class TransactionModel
+    {
+        public Guid TransactionID { get; set; }
+        public Guid UserID { get; set; }
+        public Guid ListingID { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
