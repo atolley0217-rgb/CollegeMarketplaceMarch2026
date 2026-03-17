78d7f61586c11c7baf5565aa0663363d1b437f23
Jacob Groves <jgroves0404@starkstate.net>
2026-03-17 13:32:46 -0400

    Created Database service folder, database service.cs and the controller, as anthony requested

---
+++ Controllers/DatabaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollegeMarketplaceMarch2026.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        private readonly DatabaseServices _db = new DatabaseServices();
+
+        public ActionResult Users()
+        {
+            var users = _db.GetAllUsers();
+            return View(users);
+        }
+
+        public ActionResult Listings()
+        {
+            var listings = _db.GetAllListings();
+            return View(listings);
+        }
+    }
+}
---
+++ Services/DatabaseServices.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using CollegeMarketplaceMarch2026.Models;   // adjust to your namespace
+
+namespace CollegeMarketplaceMarch2026.Services
+{
+
+}
+public class DatabaseServices
+{
+    private readonly string _connectionString;
+
+    public DatabaseServices()
+    {
+        _connectionString = ConfigurationManager.ConnectionStrings["MarketplaceDB"].ConnectionString;
+    }
+
+    // ---------------------------
+    // Get All Users
+    // ---------------------------
+    public List<UserModel> GetAllUsers()
+    {
+        var users = new List<UserModel>();
+
+        using (SqlConnection conn = new SqlConnection(_connectionString))
+        {
+            conn.Open();
+
+            string query = "SELECT UserID, FirstName, LastName, Email, PhoneNumber, Password FROM Users";
+
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    users.Add(new UserModel
+                    {
+                        UserID = reader.GetGuid(0),
+                        FirstName = reader.GetString(1),
+                        LastName = reader.GetString(2),
+                        Email = reader.GetString(3),
+                        PhoneNumber = reader.GetString(4),
+                        Password = reader.GetString(5)
+                    });
+                }
+            }
+        }
+
+        return users;
+    }
+
+    // ---------------------------
+    // Get All Listings
+    // ---------------------------
+    public List<ListingModel> GetAllListings()
+    {
+        var listings = new List<ListingModel>();
+
+        using (SqlConnection conn = new SqlConnection(_connectionString))
+        {
+            conn.Open();
+
+            string query = @"SELECT ListingID, UserID, ItemName, ItemDesc, ItemType, SellPrice, DateListed 
+                             FROM Listing";
+
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    listings.Add(new ListingModel
+                    {
+                        ListingID = reader.GetGuid(0),
+                        UserID = reader.GetGuid(1),
+                        ItemName = reader.GetString(2),
+                        ItemDesc = reader.GetString(3),
+                        ItemType = reader.GetString(4),
+                        SellPrice = reader.GetDecimal(5),
+                        DateListed = reader.GetDateTime(6)
+                    });
+                }
+            }
+        }
+
+        return listings;
+    }
+}
